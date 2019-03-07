0bafca7ba8f84e5f3890b78159bc4b77b4df68fa
xoyabc <1031138448@qq.com>
2019-03-07 23:07:14 +0800

    Update clean_old_log.sh

--- clean_old_log.sh
+++ clean_old_log.sh
@@ -3,7 +3,7 @@
 DAY_DELETE_1="30"
 # delete old files,just save files in 20 days
 DAY_DELETE_2="20"
-# delete old files,just save files in 60 days
+# delete old files,clear uncut log file, just save files in 60 days
 DAY_DELETE_3="60"
 declare -a APP_LOG_DIR_1
 APP_LOG_DIR_1=(
@@ -26,7 +26,7 @@ function clean_old_logfile_1()
     # clean log file
     for dir in ${APP_LOG_DIR_1[*]}
     do
-        [ -d ${dir} ] && find ${dir} -regextype posix-extended -regex '.*\.(log|gz).?([0-9]{1,2}){0,1}' -type f -mtime +${DAY_DELETE_1} -delete
+        [ -d ${dir} ] && find ${dir} -regextype posix-extended -regex '.*\.(log|gz).*' -type f -mtime +${DAY_DELETE_1} -delete
     done
 }
 function clean_old_logfile_2()
@@ -34,7 +34,7 @@ function clean_old_logfile_2()
     # clean log file
     for dir in ${APP_LOG_DIR_2[*]}
     do
-        [ -d ${dir} ] && find ${dir} -regextype posix-extended -regex '.*\.(log|gz).?([0-9]{4}-[0-9]{1,2}-[0-9]{1,2}){0,1}' -type f -mtime +${DAY_DELETE_2} -delete
+        [ -d ${dir} ] && find ${dir} -regextype posix-extended -regex '.*\.(log|gz).*' -type f -mtime +${DAY_DELETE_2} -delete
     done
 }
 function clean_old_logfile_3()
