03cac95b2b56ceb7081495af5f86a858f8a8f6dc
xoaybc <lxh1031138448@gmail.com>
2019-03-28 23:57:24 +0800

    add a sample of two-dimensional array

--- array.sh
+++ array.sh
@@ -6,7 +6,9 @@
 # * Filename      : array.sh
 # * Description   : more than one element
 # **********************************************************
-# REF: https://www.jianshu.com/p/543fa9df3469
+# REF:
+# https://www.jianshu.com/p/543fa9df3469
+# https://blog.csdn.net/ee230/article/details/48316317 
 ip_array=(
 200 ZR
 201 ZR
@@ -26,3 +28,17 @@ do
         echo "ip is $ip, role is $role"
 done
 
+# two-dimensional array 
+declare -a TEST
+TEST=(
+'1 2'
+'3 4'
+'5 6'
+)
+for i in "${TEST[@]}"
+do  
+    b=($i)
+    url=${b[0]}
+    host=${b[1]}
+    echo "url:$url,host:${host}"
+done
