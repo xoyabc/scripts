96e7b4e88ab9cf9625e08f2c4e7ff93a716bbb6b
xoyabc <lxh1031138448@gmail.com>
2020-07-20 00:38:14 +0800

    Update write_csv_dictionary.py
    add memo column

--- movie/siff2019/write_csv_dictionary.py
+++ movie/siff2019/write_csv_dictionary.py
@@ -12,10 +12,10 @@ with open("json.txt", 'rU') as f:
     movie_info_list = []
     data = json.load(f)
     print type(data)
-    f1 = open('siff2019.csv', 'w')
+    f1 = open('siff2020.csv', 'w')
     f1.write(codecs.BOM_UTF8)
     with f1:
-        fnames = ['group', 'area', 'halls_name', 'country', 'cinema', 'projection_material', 'etime', 'director', 'show_time', 'length', 'film_medium', 'weekday', 'date', 'film2', 'film1', 'show_type', 'resolving_power', 'id', 'co_production_country', 'stime']
+        fnames = ['group', 'area', 'halls_name', 'country', 'cinema', 'projection_material', 'etime', 'director', 'show_time', 'length', 'film_medium', 'weekday', 'date', 'film2', 'film1', 'show_type', 'resolving_power', 'id', 'co_production_country', 'stime', 'memo']
         writer = csv.DictWriter(f1, fieldnames=fnames)
         writer.writeheader()
         #for i,v in enumerate(data[0:10]):
