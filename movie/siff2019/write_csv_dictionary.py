1e84f72cb6c3e16afc88476d3f41684883035f9b
xoyabc <lxh1031138448@gmail.com>
2021-05-22 02:11:15 +0800

    Update write_csv_dictionary.py
    adjust filed order

--- movie/siff2019/write_csv_dictionary.py
+++ movie/siff2019/write_csv_dictionary.py
@@ -7,15 +7,19 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+f1 = open('siff2020.csv', 'w')
+f1.write(codecs.BOM_UTF8)
+
 
 with open("json.txt", 'rU') as f:
     movie_info_list = []
     data = json.load(f)
     print type(data)
-    f1 = open('siff2020.csv', 'w')
-    f1.write(codecs.BOM_UTF8)
+
     with f1:
-        fnames = ['group', 'area', 'halls_name', 'country', 'cinema', 'projection_material', 'etime', 'director', 'show_time', 'length', 'film_medium', 'weekday', 'date', 'film2', 'film1', 'show_type', 'resolving_power', 'id', 'co_production_country', 'stime', 'memo']
+        # fieldnames must contains all fields in dict, or it will throw an exception:
+        # " ValueError: dict contains fields not in fieldnames: u'show_time' "
+        fnames = ['group', 'id', 'film1', 'film2', 'date', 'weekday', 'stime', 'etime', 'length', 'cinema', 'halls_name', 'director', 'country', 'co_production_country', 'resolving_power', 'projection_material', 'film_medium', 'show_type', 'memo', 'area', 'show_time']
         writer = csv.DictWriter(f1, fieldnames=fnames)
         writer.writeheader()
         #for i,v in enumerate(data[0:10]):
