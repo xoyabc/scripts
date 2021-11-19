a12c529a6978396c7d734eb686e048fdde91c1d4
xoyabc <lxh1031138448@gmail.com>
2021-11-19 16:52:56 +0800

    remove port when the port is 80

--- generate_url_link/generate_url_link.sh
+++ generate_url_link/generate_url_link.sh
@@ -8,8 +8,8 @@
 # **********************************************************
 
 
-WEB_ROOT_DIR="/www"
-PORT="8080"
+WEB_ROOT_DIR="/export/servers/nginx/html"
+PORT="80"
 WEB_ROOT_DIR_ESP=$(echo ${WEB_ROOT_DIR} |sed 's#/#\\/#g')
 CURRENT_DIR="${PWD}"
 IP=$(ip a|awk --re-interval '/(eth|ens[0-9]+)/ && $0!~/\/32/ && $2~/^10.|^192.168.|^172.(1[6-9]|2[0-9]|3[0-1])/{split($2,a,"/");print a[1]}')
@@ -35,7 +35,12 @@ function gen_link ()
         # deal with absolute path
         then
             URI=$(echo ${FILE} |sed -r "s/^${WEB_ROOT_DIR_ESP}(.*)$/\1/")
-            URL="http://${IP}:${PORT}${URI}"
+            if [ ${PORT} -eq 80 ]
+            then
+                URL="http://${IP}${URI}"
+            else
+                URL="http://${IP}:${PORT}${URI}"
+            fi
         # deal with relative path which starts with "../../" or “../” or ../../../dir1/dir2/filename
         # such as ../../filename
         elif `echo ${FILE} |grep -Eq "^\.\."`
@@ -47,11 +52,21 @@ function gen_link ()
             REAL_DIR_NAME_POST=$(dirname ${FILE} |sed -r 's#(\.\.\/|\.\.)##g')
             [ -z ${REAL_DIR_NAME_POST} ] && REAL_DIR_NAME="${REAL_DIR_NAME_PRE}" || REAL_DIR_NAME="${REAL_DIR_NAME_PRE}/${REAL_DIR_NAME_POST}"
             URI=$(echo ${REAL_DIR_NAME} |sed "s/${WEB_ROOT_DIR_ESP}//g")
-            URL="http://${IP}:${PORT}${URI}/${FILENAME}"
+            if [ ${PORT} -eq 80 ]
+            then
+                URL="http://${IP}${URI}/${FILENAME}"
+            else
+                URL="http://${IP}:${PORT}${URI}/${FILENAME}"
+            fi
         # deal with relative path 
         else
             URI=$(echo ${CURRENT_DIR} |sed "s/${WEB_ROOT_DIR_ESP}//g")
-            URL="http://${IP}:${PORT}${URI}/${FILE}"
+            if [ ${PORT} -eq 80 ]
+            then
+                URL="http://${IP}${URI}/${FILE}"
+            else
+                URL="http://${IP}:${PORT}${URI}/${FILE}"
+            fi
         fi
         
         echo ${URL}
@@ -76,17 +91,17 @@ main(){
             # 2, absolute directory
             if [ -d ${FILENAME} ] && ( `echo ${FILENAME} |grep -Eq "^${WEB_ROOT_DIR}"` || `echo ${CURRENT_DIR} |grep -Eq "^${WEB_ROOT_DIR}"` )
             then
-		FILE_NUM=$(find ${FILENAME} -type f |wc -l)
-		# output file download link if the dir has more than one file
-		if [ ${FILE_NUM} -ge 1 ]
-		then
+                FILE_NUM=$(find ${FILENAME} -type f |wc -l)
+                # output file download link if the dir has more than one file
+                if [ ${FILE_NUM} -ge 1 ]
+                then
                     find ${FILENAME} -type f |while read file
                     do
                         gen_link ${file}
                     done
-		else
-		    warn_echo "There is no file under ${FILENAME} directory"
-		fi 
+                else
+                    warn_echo "There is no file under ${FILENAME} directory"
+                fi 
             else
                 gen_link ${FILENAME}
             fi
