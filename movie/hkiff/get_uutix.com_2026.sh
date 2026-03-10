9e53a80c0a3310474acb555415223686a76d15a9
xoyabc <1031138448@qq.com>
2026-03-11 00:27:14 +0800

    change file format to gb18030

--- movie/hkiff/get_uutix.com_2026.sh
+++ movie/hkiff/get_uutix.com_2026.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT_FILE="output.csv"
+
 # pageNo=1&pageSize=1000
 # curl 'https://www.uutix.com/api/oversea/projectGroup/hkiffpb2026/getProjectGroupDataList?t=1773158837987&WuKongReady=h5&projectGroupId=13&pageNo=1&pageSize=1000'
 
@@ -7,4 +9,7 @@ jq -r '
   ["projectName", "projectStartTime", "venueName"],
   (.data[].projectList[] | [.projectName, .projectStartTime, .venueName])
   | @csv
-' /tmp/1 > output.csv
+' /tmp/1 > ${OUTPUT_FILE}
+
+/usr/bin/iconv -f utf-8 -t gb18030 ${OUTPUT_FILE} -o movie.csv
+echo "转换完成，CSV 保存为 movie.csv"
